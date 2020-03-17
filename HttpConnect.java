49611457285166547d6b02b4b74e5725ea6708f9
nive1206 <nivesingh1206@gmail.com>
2020-03-17 18:56:56 +0530

    Add files via upload

--- HttpConnect.java
+++ HttpConnect.java
@@ -1,102 +1,56 @@
 package com.gsc.boc.brm.util;
+
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHeaders;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.mime.HttpMultipartMode;
-import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.entity.mime.content.FileBody;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.json.simple.parser.JSONParser;
+import org.apache.commons.io.IOUtils;
 
 import com.gsc.boc.brm.entity.JenkinsJob;
 
-import org.apache.james.mime4j.message.SingleBody;
+public class HttpConnect {
+	
 
-
-public class HttpConnect  {
-	//private static String crumb;
-
-	public static String sendGetRequest(String url, String authorization,JenkinsJob job) {
-		URLConnection urlConn = null;
-		 String crumb="";
-		String inputLine	="";
-		String Jenkinscrumb="";
+	public static String sendGetRequest(String url, JenkinsJob job) {
+		
+		
 		try {
-			//JenkinsJob job=new JenkinsJob();
-		JSONParser parser = new JSONParser();
-		
-			/*
-			 * HttpPost request = new HttpPost(url); String authHeader = "Basic " +
-			 * authorization; request.setHeader(HttpHeaders.AUTHORIZATION, authHeader);
-			 */
 			
-			  URL url1 = new URL(url);
-			  
-			  urlConn = url1.openConnection(); urlConn.setRequestProperty("Authorization",
-			  "Basic " + authorization);
-			 
-		
-		BufferedReader buff = new BufferedReader(new InputStreamReader(
-				urlConn.getInputStream()));
-		
-		   while ((inputLine = buff.readLine()) != null) 
-			// System.out.println(inputLine);
-		   Jenkinscrumb=inputLine.toString();
-		   System.out.println(Jenkinscrumb);
-       
-		   String[] arrOfStr = Jenkinscrumb.split(",", -2); 
-		   System.out.println(arrOfStr[1]);  
-		   String crumb1 = arrOfStr[1].substring(1);
-          crumb1 = crumb1.split(":")[1];
-          System.out.println(crumb1);
-         crumb=crumb1.replace("\"","");
-         System.out.println(crumb);
-		   //return crumb;
-		   
-		//curl -s -XPOST 'http://example.com/createItem?name=yourJobName' -u username:API_TOKEN --data-binary @mylocalconfig.xml -H "Content-Type:text/xml"
-		
-         
-         //String[] command = {"curl -s -XPOST " +job.getUrl()+"/createItem?name="+job.getJobname()+"-u"+job.getUsername()+":"+ job.getToken() + "--data-binary" + job.getXmlFilePath()+ crumb+  "'content-Type:text/xml'"}; 
-		//curl -s -XPOST 'http://129.146.59.178:8080/createItem?name=yourdemo' -u devops:11dc8f66300e9e3550f66f0bb22f1fbf38 --data-binary @mylocalconfig.xml -H a915c770d8bad738c5ce8118d4b2384c8d78bfe52e76b4024ddf2398b256f86a -H "Content-Type:text/xml"
- 		String command = "curl -s -XPOST " +job.getUrl()+"/createItem?name="+job.getJobname()+ "-u" +job.getUsername()+":"+ job.getToken() +  "--data-binary"  + job.getXmlFilePath()  +  crumb +   "'content-Type:text/xml'"; 
-
-         System.out.println(command);
-		ProcessBuilder process = new ProcessBuilder(command);
-		Process p;
-		
-			p = process.start();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			StringBuilder builder = new StringBuilder();
-			String line = null;
-			while ((line = reader.readLine()) != null) {
-				builder.append(line);
-				builder.append(System.getProperty("line.separator"));
-			}
-			String result = builder.toString();
-			System.out.print(result);
-
+			String crumbkey = "curl -s -XPOST \"" + job.getUrl() + "/crumbIssuer/api/xml"+"?"+"xpath=//crumb" + "\" -u "+ job.getUsername() + ":" + job.getToken();
+			
+			//String crumbkey = "curl -s -X POST \"http://129.146.59.178:8080/crumbIssuer/api/xml?xpath=//crumb\" -u devops:11dc8f66300e9e3550f66f0bb22f1fbf38";
+			System.out.println(crumbkey);
+			
+			Process process = Runtime.getRuntime().exec(crumbkey);
+			InputStream inputStream = process.getInputStream();
+			String result = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+			String result1 = result.substring(result.indexOf(">") + 1);
+			String finalcrumb = result1.substring(0, result1.indexOf("<"));
+			System.out.println(result+"-------"+finalcrumb);
+						
+			String command = "curl -s -XPOST \"" + job.getUrl() + "/createItem?name=" + job.getJobname() + "\" -u "
+					+ job.getUsername() + ":" + job.getToken() + " --data-binary @" + job.getXmlFilePath() + " -H "
+					+ "Jenkins-Crumb:"+ finalcrumb + " -H content-Type:text/xml";
+			
+			System.out.println(command);
+			
+			Process processjob = Runtime.getRuntime().exec(command);
+			InputStream inputStreamjob = processjob.getInputStream();
+			String output = IOUtils.toString(inputStreamjob, StandardCharsets.UTF_8);
+			System.out.println(output);
 		} catch (IOException e) {
 			System.out.print("error");
 			e.printStackTrace();
 		}
-	
-		
-		return crumb;
 
-	
-}
+		
+		return url;
+
+	}
 	/*
 	 * public static void sendPostRequest (String url, String xmlFilePath, String
 	 * authorization, String crumb) {
@@ -129,52 +83,47 @@ public class HttpConnect  {
 	 * System.out.println(response.toString()); } catch (IOException e) {
 	 * e.printStackTrace(); } }
 	 */
-	
-	
-	
-	
-	/*public void test(){
-		
-		String auth = DEFAULT_USER + ":" + DEFAULT_PASS;
-		byte[] encodedAuth = Base64.encodeBase64(
-		  auth.getBytes(StandardCharsets.ISO_8859_1));
-		String authHeader = "Basic " + new String(encodedAuth);
-		//request.setHeader(HttpHeaders.AUTHORIZATION, authHeader);
-		 
-		HttpClient client = HttpClientBuilder.create().build();
-		HttpResponse response = client.execute(request);
-		 
-		int statusCode = response.getStatusLine().getStatusCode();
-		assertThat(statusCode, equalTo(HttpStatus.SC_OK));
-	}*/
 
 
-  public static void uploadCred(JenkinsJob job) {
-  
-  try { 
-	  String credentials = Authenticator.getAuthToken(job.getUsername(),job.getToken()); 
-  
- String crumb = HttpConnect.sendGetRequest(job.getUrl()+"/crumbIssuer/api/json",credentials, job);
- 
-  String[] command = {"curl -X POST " + " \\ -U"+ job.getUsername()+":"+
-  job.getToken() + "\\ -H" + crumb+ "\\ -H"+
-  "'content-type:application/xml' \\ -d" +job.getUploadCred()+job.getUrl()+
-  "/job/top-folder/job/sub-folder/credentials/store/folder/domain/_/createCredentials"
-  };
-  
-  
-  //curl -X POST \ -u $JENKINS_USER:$JENKINS_PASSWORD_OR_API_TOKEN \ -H "Jenkins-Crumb:${JENKINS_CRUMB}" \ -H 'content-type:application/xml' \ -d @credential.xml \"$JENKINS_URL/job/top-folder/job/sub-folder/credentials/store/folder/domain/_/createCredentials"
-  ProcessBuilder process = new ProcessBuilder(command); Process p;
-  
-  p = process.start(); BufferedReader reader = new BufferedReader(new
-  InputStreamReader(p.getInputStream())); StringBuilder builder = new
-  StringBuilder(); String line = null; while ((line = reader.readLine()) !=
-  null) { builder.append(line);
-  builder.append(System.getProperty("line.separator")); } String result =
-  builder.toString(); System.out.print(result);
-  
-  } catch (IOException e) { System.out.print("error"); e.printStackTrace(); }
- 
-	}
+
+		public static String uploadCred(String url,JenkinsJob job) {
+			try {
+String crumbkey = "curl -s -XPOST \"" + job.getUrl() + "/crumbIssuer/api/xml"+"?"+"xpath=//crumb" + "\" -u "+ job.getUsername() + ":" + job.getToken();
+			
+			//String crumbkey = "curl -s -X POST \"http://129.146.59.178:8080/crumbIssuer/api/xml?xpath=//crumb\" -u devops:11dc8f66300e9e3550f66f0bb22f1fbf38";
+			System.out.println(crumbkey);
+			
+			Process process = Runtime.getRuntime().exec(crumbkey);
+			InputStream inputStream = process.getInputStream();
+			String result = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+			String result1 = result.substring(result.indexOf(">") + 1);
+			String finalcrumb = result1.substring(0, result1.indexOf("<"));
+			System.out.println(result+"-------"+finalcrumb);
+				 
+			String command = "curl -XPOST \\ -u " + job.getUsername()+":"+ job.getToken() 
+			  + " \\ -H \"Jenkins-Crumb:{" + finalcrumb + "}\" "
+			  + "\\ -H  'content-type:application/xml' "
+			 + "\\ -d @" + job.getUploadCred()
+			+ " \\ \""+job.getUrl()+"credentials/store/system/domain/_/createCredentials\"";
+			    
+			     
+				 
+
+			     System.out.println(command);
+				ProcessBuilder process1 = new ProcessBuilder(command);
+				Process p;
+				
+				Process processjob = Runtime.getRuntime().exec(command);
+				InputStream inputStreamjob = processjob.getInputStream();
+				String output = IOUtils.toString(inputStreamjob, StandardCharsets.UTF_8);
+				System.out.println(output);
+			} catch (IOException e) {
+				System.out.print("error");
+				e.printStackTrace();
+			}
+			return url;
+
+			 
+			 
+			}
 }
-
